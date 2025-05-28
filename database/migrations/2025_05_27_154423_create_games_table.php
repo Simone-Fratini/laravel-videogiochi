8bc3c24b65bdac2693a34743d21c1e03df38c1ff
Simone-Fratini <simone@fratini.com>
2025-05-28 12:48:43 +0200

    Add Game, Genre, and Platform models with corresponding migration files

---
+++ database/migrations/2025_05_27_154423_create_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_rawg')->unique();
+            $table->string('slug');
+            $table->string('name');
+            $table->date('released')->nullable();
+            $table->string('background_image')->nullable();
+            $table->float('rating')->nullable();
+            $table->integer('playtime')->nullable();
+            $table->text('description')->nullable();
+            $table->string('esrb_rating')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('games');
+    }
+};
